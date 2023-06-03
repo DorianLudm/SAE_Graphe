49e990fa91a548b901f47863053ff30398a2ca34
DorianLudm <122395205+DorianLudm@users.noreply.github.com>
2023-06-03 15:00:43 +0200

    Recup fichier SAE

--- SAE.py
+++ SAE.py
@@ -112,7 +112,7 @@ def collaborateurs_proches(G,u,k):
 #     while act2 not in acteur:
 #         collaborateurs_directs = set()
 #         for c in acteur:
-#             for voisin in G.adj[c]:
+#             for voisin in G[c]:
 #                 if voisin not in acteur:
 #                     collaborateurs_directs.add(voisin)
 #         acteur = acteur.union(collaborateurs_directs)
@@ -151,4 +151,40 @@ def centralite(graph):
         if centralite_a < ppc:
             ppc = centralite_a
              
+lePlusAuCentre = centralite(G)
+
+
+
+def plusGrandeDistanceEntreActeurs(graph):
+    visited = []
+    for acteur in G.nodes():
+        depart = acteur
+        break
+    #On cherche un acteur sur l'extremite du graphe
+    queue = [depart]
+    node = depart
+    while queue:
+        node = queue.pop(0)
+        if node not in visited:
+            visited.append(node)       
+            for edge in graph.edges:
+                if edge[0] == node:
+                    queue.append(edge[1])
+                elif edge[1] == node:
+                    queue.append(edge[0])
+    #On cherche la distance avec l'extremite la plus eloignee
+    # queue2 = [node]
+    # i = -1
+    # while queue2:
+    #     node2 = queue2.pop(0)
+    #     if node2 not in visited:
+    #         visited.append(node2)
+    #         i += 1         
+    #         for edge in graph.edges:
+    #             if edge[0] == node2:
+    #                 queue2.append(edge[1])
+    #             elif edge[1] == node2:
+    #                 queue2.append(edge[0])
+    # return i
+    return centralite_acteur(G, node)
 # %%
